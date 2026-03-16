da1ecbb8b827eff9c35a685e71c4daf74cab8519
Chandan <chandan.dsa@gmail.com>
2026-03-16 08:28:35 +0530

    feat: update font settings to use 'Inter' with system-ui for improved consistency and add Google Font URLs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
   title: "SpotCove - Public Frontend",
@@ -15,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={inter.variable}>
-      <body className={inter.className}>{children}</body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
